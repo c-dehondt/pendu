2c2319da6c0bacb91badfb33098dbde97e58cbba
c.dehondt <c.dehondt@live.com>
2017-07-03 16:30:01 +0200

    finish project the 'pendu' step 1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var mots= ["sac"];
 var ordi= mots [Math.trunc(Math.random() * mots.length)];
-console.log(ordi);
+var lettre=[];
+var scoreJ=0;
+var nbEssai=0;
+var ok=0;
 
 
 var resultat = "";
 for (var i = 0; i < ordi.length; i++) {
-  resultat += "_";
+  resultat += "-";
 }
 
 function choixOrdi(){
@@ -19,18 +22,37 @@ function jouer() {
 
   for(var j=0; j<ordi.length; j++){
   if(joueur == ordi[j]){
-
     resultat= resultat.split("");
-
     resultat[j]=ordi[j];
-
     resultat= resultat.join("");
+    ok++;
     document.getElementById("affichage").innerHTML= resultat;
-
-    }
   }
+ }
+
+  if(ok <1){
+    lettre.push(joueur);
+    document.getElementById("null").innerHTML= lettre;
+    nbEssai++
+    document.getElementById("essai").innerHTML= nbEssai;
+    ok=0;
+}else {
+  lettre.push(joueur);
+  document.getElementById("null").innerHTML= lettre;
+  ok=0;
 }
-//
-//,"bonjour", "salut", "hello","chaise", "bureau", "table"
-//
-//
+
+    if (resultat == ordi) {
+      alert("tu as gagné, bravo!!")
+      reset();
+
+    }else if(nbEssai >= 10){
+      alert("tu as perdu" + "  le mot"+ "   " + ordi)
+      reset();
+    }document.getElementById("essai").innerHTML= nbEssai;
+}
+
+    function reset() {
+      nbEssai =0;
+      window.location.reload();
+    }
